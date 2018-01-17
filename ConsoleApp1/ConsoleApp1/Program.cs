bc9c314beeca91e0dfefe9e53a25e685908c356f
Harshu1909 <harsh.coderkube@gmail.com>
2018-01-17 16:32:32 -0500

    C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------Lab Task1--------");
+           
+            Console.WriteLine("Which Country you flied from:");
+            String country = Console.ReadLine();
+
+            Console.WriteLine("How many bags you has with you:");
+            String bags = Console.ReadLine();
+
+
+            Console.WriteLine("How many the total weight of your luggage:");
+            String weight = Console.ReadLine();
+
+
+            int avg = int.Parse(weight) / int.Parse(bags);
+
+          
+            
+            Console.WriteLine("I flew from "+country+" having "+bags+" bags with average weight of "+ avg);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
